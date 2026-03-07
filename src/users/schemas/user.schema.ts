b287eaf7af4f0f85efef89d3b67ac61d08bcc3aa
talaf <artemk2504@gmail.com>
2026-03-07 18:38:01 +0000

    refactor:remove refresh token field and location folder

--- src/users/schemas/user.schema.ts
+++ src/users/schemas/user.schema.ts
@@ -13,9 +13,6 @@ export class User {
 
   @Prop({ required: true })
   name: string;
-
-  @Prop({ default: null })
-  refreshToken?: string;
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
